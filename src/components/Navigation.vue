99dce34890eeff8f179eba39441a22ec90906399
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-09-22 10:53:25 -0500

    adjusted title to include '3'

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -4,7 +4,7 @@
       <f7-nav-left>
         <i class="f7-icons e3i-gray" @click="togglePanel()" style="margin-left: 10px;">bars</i>
       </f7-nav-left>
-      <f7-nav-center sliding="" class="e3i-blue nav-title">Eco Metrics</f7-nav-center>
+      <f7-nav-center sliding="" class="e3i-blue nav-title">Eco<span style="font-size:11px !important;">3</span> Metrics</f7-nav-center>
       <f7-nav-right></f7-nav-right>
     </f7-navbar>
   </div>
