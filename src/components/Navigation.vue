d167abd2da5cbffe69d79fed1090ee5e23c09a25
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-09-26 15:19:12 -0500

    made panel launch container to make bigger tappable target

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,9 @@
   <div>
     <f7-navbar>
       <f7-nav-left>
-        <i class="f7-icons e3i-gray" @click="togglePanel()" style="margin-left: 10px;">bars</i>
+        <div class="panel-launcher" @click="togglePanel">
+          <i class="f7-icons e3i-gray" style="margin-left: 10px;">bars</i>
+        </div>
       </f7-nav-left>
       <f7-nav-center sliding="" class="e3i-blue nav-title">Eco<span style="font-size:11px !important;">3</span> Metrics</f7-nav-center>
       <f7-nav-right></f7-nav-right>
