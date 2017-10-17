c3d0a65408047cc0ab369bb4f441a929e3d2f9d1
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-10-17 17:36:51 -0500

    added more logic to list filter in requestUnit()

--- src/home.vue
+++ src/home.vue
@@ -108,8 +108,8 @@ export default {
           }
         }
       },100);
-      //remove after ble is obsolete
-      list = list.filter(x => x.indexOf('ecoice') != -1)
+      //remove filter after ble is obsolete
+      list = list.filter(unit => unit.name.toLowerCase().indexOf('ecoice') != -1)
       window.tmptimeout = window.setTimeout(function() { window.clearInterval(window.tmpinterval);  },10000);
     },
     togglePanel: function() {
