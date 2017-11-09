04aee3488f39e902c06cee875b97d02c0b247d8a
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-11-09 10:29:41 -0600

    first commit consumption display component

---
+++ src/components/Consumption.vue
@@ -0,0 +1,121 @@
+<template lang="html">
+  <div>
+    <h3 class="page-title">consumption</h3>
+    <!-- Daily Consumption Block Start -->
+    <div class="content-block inset">
+      <div class="content-block-inner">
+        <div class="e3i-content-lg">
+          <div class="status-container">
+            <!-- Default Daily Consumption-->
+            <div v-if="consumption === '---'" class="status-wrapper">
+              <p class="status-text-blank e3i-gray-light" style="letter-spacing: 1px;">---</p>
+            </div>
+            <!-- Daily Consumption is loading -->
+            <div v-else-if="consumption === 'loading'" class="status-wrapper">
+              <div class="preloader-health">
+                <div class="preloader preloader-gray" style="width: 25px; height: 25px;"></div>
+              </div>
+            </div>
+            <!-- Daily Consumption # -->
+            <div v-else class="status-wrapper">
+              <p class="consumption-text e3i-success">{{ consumption }}</p>
+              <img class="status-icon-inline" src="static/images/consumption/lbs.png">
+            </div>
+          </div>
+          <div class="status-title-wrapper">
+            <span class="box-title">daily consumption</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Monthly Avg Consumption Block Start -->
+    <div class="content-block inset" >
+      <div class="content-block-inner">
+        <div class="e3i-content-sm flex">
+          <div class="avg-consumption-wrapper__title">
+            <div class="">
+              <div class="box-title-left">
+                <div>
+                  <p class="avg-consumption__title">monthly </br>average </br>consumption</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="avg-consumption-wrapper__display">
+            <div class="avg-consumption-display__top flex">
+              <!-- Average Monthly Consumption blank-->
+              <div v-if="averageConsumption === '---'">
+                <span class="avg-consumption-number e3i-gray-light">---</span>
+              </div>
+              <!-- Average Monthly Consumption Loading-->
+              <div v-else-if="averageConsumption === 'loading'">
+                <div class="preloader-health" style="left:10px; top: 10px;">
+                  <div class="preloader preloader-gray" style="width: 25px; height: 25px;"></div>
+                </div>
+              </div>
+              <!-- Average Monthly Consumption #-->
+              <div v-else style="padding-top: 7px;">
+                <span class="avg-consumption-number e3i-success">{{ averageConsumption }}</span>
+                <img class="avg-consumption-icon" src="static/images/consumption/lbs.png">
+                <p class="avg-consumption-display__daily">daily</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Monthly Avg Duty Cycle Block Start -->
+    <div class="content-block inset" >
+      <div class="content-block-inner">
+        <div class="e3i-content-sm flex">
+          <div class="avg-consumption-wrapper__title">
+            <div class="">
+              <div class="box-title-left">
+                <div>
+                  <p class="avg-consumption__title">monthly average </br>duty cycle</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="avg-consumption-wrapper__display">
+            <div class="avg-consumption-display__top flex">
+              <!-- Average Monthly Duty cycle Loading-->
+              <div v-if="averageDuty === 'loading'">
+                <div class="preloader-health" style="left:10px; top: 10px;">
+                  <div class="preloader preloader-gray" style="width: 25px; height: 25px;"></div>
+                </div>
+              </div>
+              <!-- Average Monthly Duty cycle #-->
+              <div v-else style="padding-top: 7px;">
+                <PercentageChart
+                  :averageDuty="averageDuty"
+                ></PercentageChart>
+                <!-- <span class="avg-consumption-number e3i-success">{{ averageDuty }}</span> -->
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PercentageChart from '../components/PercentageChart.vue'
+export default {
+  components:{
+    PercentageChart
+  },
+  props: {
+    consumption: String,
+    averageDuty:String,
+    averageConsumption: String
+  },
+  data: function() {
+    return {
+
+    }
+  }
+}
+</script>
