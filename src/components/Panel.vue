f4abc2931a548cbff207d40da6ea365a48a05e8b
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-11-15 14:13:41 -0600

    added according containing rename, capacity, and measurment functionality

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,11 +1,12 @@
 <template lang="html">
-  <div>
+  <div @accordion:close="clearInputs()">
     <div :hidden="!isPanelOpened" class="e3i-prompt-overlay" @click="closePanel"></div>
     <f7-panel left cover :opened='isPanelOpened'>
       <div class="menu-logo-container">
         <img class="menu-logo" src="static/images/franke-logo.jpg" alt="Franke">
       </div>
       <f7-list>
+        <!--Connect Prompt -->
         <f7-list-item
           title="Connect to machine"
           class="menu-item"
@@ -13,13 +14,15 @@
         >
           <i class="f7-icons menu-icon">social_rss_fill</i>
         </f7-list-item>
+        <!--Reset Prompt Disabled -->
         <f7-list-item
-          v-if="selectedUnitIndex == -1"
+          v-if="unitNotSelected"
           title="Reset Unit"
           class="menu-item disabled"
         >
           <i class="f7-icons menu-icon-reset disabled">refresh</i>
         </f7-list-item>
+        <!--Reset Prompt Enabled -->
         <f7-list-item
           v-else
           title="Reset Unit"
@@ -28,23 +31,104 @@
         >
           <i class="f7-icons menu-icon-reset">refresh</i>
         </f7-list-item>
-        <f7-list-item>
-        <f7-accordion>
-        <f7-accordion-item class="menu-item list-block measurements">
-          <f7-accordion-toggle>Measurements
-            <img src="static/images/ruler.png" class="menu-icon-img">
-          </f7-accordion-toggle>
-          <f7-accordion-content>
-            <div class="menu-drop-down flex">
-              <p :class="{'menu-measurement': true, disabled: !this.imperial}">Lbs</p>
-              <f7-input type="switch" @click="switchMeasurement"></f7-input>
-              <p :class="{'menu-measurement': true, disabled: this.imperial}">Kgs</p>
-            </div>
-          </f7-accordion-content>
-        </f7-accordion-item>
-      </f7-accordion>
-      </f7-list-item>
+        <!-- Accordion Start -->
+        <f7-list-item style="padding: 0;">
+          <div class="list-block accordion-list" style="width: 100%;">
+            <!-- Rename Accordion -->
+            <li v-if="unitNotSelected" class="accordion-item">
+              <div class="item-inner" style="padding-right:0; background: none;">
+                <div class="menu-item item-title disabled" style="width:100%;">
+                  Rename Eco Unit
+                  <i class="f7-icons menu-icon" style="font-size:18px; float: right;">compose</i>
+                </div>
+              </div>
+            </li>
+            <li v-else class="accordion-item"><a href="#" class="item-link" style="padding-left:0;" @click="matchNames('rename')">
+              <div class="item-inner" style="padding-right:0; background: none;">
+                <div class="menu-item item-title" style="width:100%;">
+                  Rename Eco Unit
+                  <i class="f7-icons menu-icon" style="font-size:18px; float: right;">compose</i>
+                </div>
+              </div></a>
+              <div class="accordion-item-content">
+                <div class="content-block">
+                  <div class="menu-drop-down flex" style="justify-content: flex-start;">
+                    <div>
+                      <input id="rename"  class="menu-rename-input" type="text"
+                        v-model="newUnitName"
+                        style="margin: 5px 0; padding: 0 8px; font-weight: 300"/>
+                    </div>
+                    <button :disabled="nameTooShort || nameIsSame || unitNotSelected" class="menu-rename-button" type="button" name="button" @click="setName()">
+                      <i class="f7-icons" style="padding: 0 3px;">add</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <!--Capacity Accordion -->
+            <li v-if="unitNotSelected" class="accordion-item">
+              <div class="item-inner" style="padding-right:0; background: none;">
+                <div class="menu-item item-title disabled" style="width:100%;">
+                  Set Ice Machine Capacity
+                  <i class="f7-icons menu-icon" style="font-size:19px; float: right;">box</i>
+                </div>
+              </div>
+            </li>
+            <li v-else class="accordion-item"><a href="#" class="item-link" style="padding-left:0;" @click="focus('capacity')">
+              <div class="item-inner" style="padding-right:0; background: none;">
+                <div class="menu-item item-title" style="width:100%;">
+                  Set Ice Machine Capacity
+                  <i class="f7-icons menu-icon" style="font-size:19px; float: right;">box</i>
+                </div>
+              </div></a>
+              <div class="accordion-item-content">
+                <div class="content-block">
+                  <div class="menu-drop-down flex" style="justify-content: flex-start;">
+                    <div class="">
+                      <input id="capacity" class="menu-capacity-input" type="text"
+                        v-model="capacityValue"
+                        style="margin: 5px 0; padding: 0 8px; font-weight: 300"
+                        />
+                        <span v-if="this.imperial" style="margin: 0 5px;"> lbs</span>
+                        <span v-else style="margin: 0 5px;"> kgs</span>
+                    </div>
+                    <button :disabled="!isWholeNumber || Number(this.capacityValue) === 0  || unitNotSelected"
+                      :class="{'menu-rename-button':true, 'disabled':!isWholeNumber }"
+                      type="button" name="button"
+                      @click="setCapacity()"
+                    >
+                      <i class="f7-icons" style="padding: 0 3px;">add</i>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <!--Measurements Accordion -->
+            <li class="accordion-item"><a href="#" class="item-link" style="padding-left:0;">
+              <div class="item-inner" style="padding-right:0; background: none;">
+                <div class="menu-item item-title" style="overflow: visible;">
+                  Measurements
+                  <img src="static/images/ruler.png" class="menu-icon-img">
+                </div>
+              </div></a>
+              <div class="accordion-item-content">
+                <div class="content-block">
+                  <div class="menu-drop-down flex" style="justify-content: flex-start;">
+                    <p :class="{'menu-measurement': true, disabled: !this.imperial}">Lbs</p>
+                    <f7-input id="switch"
+                    type="switch"
+                    @click="switchMeasurement"
+                    style="width:auto"></f7-input>
+                    <p :class="{'menu-measurement': true, disabled: this.imperial}">Kgs</p>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </div>
+        </f7-list-item>
       </f7-list>
+      <p :hidden="!nameTooShort" class="e3i-fail" style="font-size: 9px; margin-left: 15px">name should be at least 3 characters long</p>
+      <p :hidden="isWholeNumber" class="e3i-fail" style="font-size: 9px; margin-left: 15px">capacity should be a whole number</p>
     </f7-panel>
   </div>
 </template>
@@ -56,7 +140,27 @@ export default {
     isPanelOpened: Boolean,
     isResetOpened: Boolean,
     selectedUnitIndex: Number,
-    imperial: Boolean
+    imperial: Boolean,
+    unitName: String
+  },
+  computed:{
+    unitNotSelected: function() {
+      return this.selectedUnitIndex === -1;
+    },
+    nameIsSame: function() {
+      return this.unitName.trim() == this.newUnitName.trim()
+    },
+    nameTooShort: function() {
+      return this.newUnitName.length < 3
+    },
+    isWholeNumber: function() {
+      let input = Number(this.capacityValue)
+      if(!isNaN(input)){
+        return (typeof input == 'number' && input%1 == 0);
+      }else{
+        return false;
+      }
+    }
   },
   methods: {
     openConnectPrompt: function() {
@@ -70,6 +174,42 @@ export default {
     },
     switchMeasurement: function() {
       this.$emit('switchMeasurements');
+    },
+    matchNames: function(id) {
+      if(this.selectedUnitIndex != -1){
+        this.newUnitName = this.unitName;
+        this.focus(id)
+      }
+    },
+    focus: function(id) {
+      document.getElementById(id).focus()
+    },
+    setName: function() {
+      this.$emit('setNewName', this.newUnitName);
+      this.clearInputs();
+    },
+    clearInputs: function()  {
+      this.newUnitName = this.unitName;
+      this.capacityValue = 0;
+    },
+    setCapacity: function() {
+      if (this.imperial) {
+        console.log('Set Capacity: ' + this.capacityValue + ' lbs')
+        this.$emit('setCapacity', this.capacityValue),
+        this.capacityValue = 0;
+      }else{
+        let toLbs = Math.round(this.capacityValue * 2.20462262185);
+        console.log(this.capacityValue + ' kgs converts to ' + toLbs + ' lbs')
+        this.$emit('setCapacity', toLbs);
+        this.capacityValue = 0;
+      }
+    }
+  },
+  data: function() {
+    return {
+      capacityValue: 0,
+      newUnitName: this.unitName,
+      warnName: false
     }
   }
 }
