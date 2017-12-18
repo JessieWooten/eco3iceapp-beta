6555942c49bb3d714f1828e3090e7ad2ec763ce5
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-12-18 14:17:03 -0600

    houses language select box. links to i18next

---
+++ src/components/menu/LangSelect.vue
@@ -0,0 +1,80 @@
+<template>
+  <div style="width: 100%;">
+    <select class="menu-lang-select" v-model="selectedLang" name="langSelect">
+      <option disabled value="" style="display: none;">{{currentLanguage(locale)}}</option>
+      <option v-for="(lang, index) in supportedLangs" :value="lang">{{currentLanguage(lang)}}</option>
+    </select>
+    <i class="f7-icons menu-icon" :disabled="isDisabled"
+    style="font-size:17px; padding: 10px 15px; margin-left: 20px;"
+    @click="setLang"
+    >chevron_right</i>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      selectedLang: '',
+      locale: 'en'
+
+    }
+  },
+  computed:{
+    supportedLangs: function() {
+      var availableLangs = [];
+      var i18n = this.$i18n.messages;
+  	  for(var key in i18n) {
+  		    availableLangs.push(key)
+  	  }
+  	  return availableLangs;
+    },
+    isDisabled: function(){
+      if(this.selectedLang === this.locale){
+        return true;
+      } else if(this.selectedLang === ''){
+        return true;
+      }else{
+        return false;
+      }
+    }
+  },
+  methods:{
+    setLang: function() {
+      this.locale = this.selectedLang;
+      this.selectedLang = '';
+      this.$emit('closePanel')
+    },
+    isSelected: function(lang) {
+      if(this.locale == lang){
+        return true;
+      }else{
+        return false;
+      }
+    },
+    currentLanguage: function(lang) {
+      switch (lang) {
+        case 'en':
+          return "English"
+          break;
+
+        case "es":
+            return "español"
+            break;
+
+        case "ja":
+          return "日本語"
+          break;
+
+        default:
+        return lang
+      }
+    }
+  },
+  watch: {
+    locale (val) {
+      this.$i18n.locale = val
+    }
+  }
+}
+</script>
