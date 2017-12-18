89a6252e33d915a0076d0f45dbf57f9ca9390b89
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-12-18 14:18:16 -0600

    added i18n initialization.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,28 @@
 import Vue from 'vue'
 import Home from './home.vue'
 import Routes from './routes.js'
+import LANG from './lang'
 import Framework7Vue from 'Framework7Vue'
+import VueI18n from 'vue-i18n'
+
 
 
 
 Vue.use(Framework7Vue);
+Vue.use(VueI18n);
+
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: LANG
+});
+
 
 export const promptBus = new Vue();
 
+
+
+// new Vue({ i18n }).$mount('#app')
 window.vue = new Vue({
   el: '#app',
   render: h => h(Home),
@@ -18,9 +32,10 @@ window.vue = new Vue({
       animateNavBackIcon: true,
       routes: Routes
   },
+  i18n,
   mounted: function(){
     window.app.inApp();
     this.$children[0].toggleConnect();
     window.app.scanBle();
   }
-})
+});
