f7debb404409dea8f23c07050eab7fcacca1e24d
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2017-12-27 12:04:59 -0600

    added 'ip changed' modal and logic

--- src/components/menu/Popup.vue
+++ src/components/menu/Popup.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div @actions:closed="closeWifiPrompt">
+  <div @actions:closed="closePopups">
     <!--Saving to unit Popup -->
     <f7-popup id="popup" :opened="popupOpened">
       <div class="flex" style="height: 100vh">
@@ -55,6 +55,14 @@
         <f7-actions-button style="width: 100%" @click="closeWifiPrompt()">{{$t("ok")}}</f7-actions-button>
       </f7-actions-group>
     </f7-actions>
+    <!-- if Ip has changed while in app -->
+    <f7-actions animated="true" :opened="ipChanged">
+      <f7-actions-group>
+        <f7-actions-label class="wifiConnect-title">{{$t("Your IP has changed")}}</f7-actions-label>
+        <f7-actions-label class="menu-item ">{{$t("Please be sure you are connected to the correct WiFi network and reconnect to your Eco3Ice unit")}}</f7-actions-label>
+        <f7-actions-button style="width: 100%" @click="closeIpPrompt()">{{$t("ok")}}</f7-actions-button>
+      </f7-actions-group>
+    </f7-actions>
   </div>
 </template>
 
@@ -65,11 +73,18 @@ export default {
     popupSaveOpened: Boolean,
     popupWasReset: Boolean,
     popupResetOpened: Boolean,
-    connectToWifi: Boolean
+    connectToWifi: Boolean,
+    ipChanged: Boolean
   },
   methods: {
     closeWifiPrompt: function() {
       this.$emit('closeWifiPrompt')
+    },
+    closeIpPrompt: function() {
+      this.$emit('closeIpPrompt')
+    },
+    closePopups: function() {
+      this.$emit('closePopups')
     }
   }
 }
