202953ad6d8d386c9a1638c8c1aa4ac739f9f533
Jessie Wooten <jessiewooten@Jessies-MacBook-Pro.local>
2018-01-03 21:18:30 -0600

    made Chart resposive. Now resizes based off of initial view width

--- src/components/PercentageChart.vue
+++ src/components/PercentageChart.vue
@@ -5,13 +5,13 @@
       <circle r="30" cx="50%" cy="50%" :stroke-dasharray="avgDutyToPercent" class="circle2" />
     </svg> -->
     <section>
-      <svg class="circle-chart" width="85" height="85" xmlns="http://www.w3.org/2000/svg">
-        <circle class="circle-chart__background" stroke="#b6b2b2" stroke-width="2" fill="none" cx="50%" cy="50%" r="35" />
-        <circle class="circle-chart__circle" stroke="#7ceced" stroke-width="2" :stroke-dasharray="avgDutyToPercent" stroke-linecap="round" fill="none" cx="50%" cy="50%" r="35" />
+      <svg class="circle-chart" width="24vw" height="24vw" xmlns="http://www.w3.org/2000/svg">
+        <circle id="percentageChart" class="circle-chart__background" stroke="#b6b2b2" stroke-width="2" fill="none" cx="50%" cy="50%" r="10vw" />
+        <circle class="circle-chart__circle" stroke="#7ceced" :stroke-dasharray="getPercentage() + ',' + getCircumference()" stroke-width="2"  stroke-linecap="round" fill="none" cx="50%" cy="50%" r="10vw" />
         <g class="circle-chart__info">
           <text v-if="averageDuty === '---'" class="circle-chart__percent" x="50%" y="40%" alignment-baseline="central" text-anchor="middle" font-size="18" fill="#b6b2b2">---</text>
-          <text v-else class="circle-chart__percent" x="50%" y="40%" alignment-baseline="central" text-anchor="middle" font-size="18" fill="#4ee038">{{ averageDuty }}%</text>
-          <text class="circle-chart__subline" x="50%" y="60%" alignment-baseline="central" text-anchor="middle" font-size="18" font-weight="300" fill="#b6b2b2">{{$t("daily")}}</text>
+          <text v-else class="circle-chart__percent" x="50%" y="40%" alignment-baseline="central" text-anchor="middle" font-size="5vw" fill="#4ee038">{{ averageDuty }}%</text>
+          <text class="circle-chart__subline" x="50%" y="60%" alignment-baseline="central" text-anchor="middle" font-size="5vw" font-weight="300" fill="#b6b2b2">{{$t("daily")}}</text>
         </g>
       </svg>
   </section>
@@ -24,19 +24,35 @@ export default {
     averageDuty: String
   },
   computed:{
-    avgDutyToPercent: function() {
-      if(this.averageDuty === '---'){
-          return "0, 220"
-      }else if(Number(this.averageDuty) === NaN){
-        return "0,220"
-      }else{
-        var percentage = 220 * (this.averageDuty * .01)
-        return percentage + ",220"
-      }
-    },
-
   },
   methods: {
+    avgDutyToPercent: function() {
+      var self = this;
+      setTimeout(function(){
+        var circumference = self.getCircumference();
+        if(self.averageDuty === '---'){
+            return ("0, " + circumference);
+        }else if(isNaN(self.averageDuty)){
+          return "0, " + circumference;
+        }else{
+          var percentage = circumference * (self.averageDuty * .01)
+          return percentage + ", " + circumference;
+        }
+      }, 200)
+    },
+    getPercentage:function(){
+      var self = this;
+        if(isNaN(self.averageDuty)){
+          return "0"
+        }else{
+          var percentage = self.getCircumference() * (self.averageDuty * .01)
+          return percentage
+        }
+    },
+    getCircumference: function() {
+        var r = document.documentElement.clientWidth / 10;
+        return String(Math.round(3.14 * (r * 2)))
+    }
   }
 }
 </script>
